a3f8aed2780ed272635d013aab5814adff3565c5
juliana <julianaclapier@gmail.com>
2021-01-18 19:35:51 -0300

    .

---
+++ entrega4.js
@@ -0,0 +1,11 @@
+var resultado = multiplica(10,prompt("Multiplico este numero por 10 ydoy el promedio"))
+
+
+function multiplica(parametro1,parametro2){
+    var resultado = ((parametro1*parametro2)/2)
+
+    return(resultado)}
+
+    alert("El resultado es = " + resultado);
+
+    
