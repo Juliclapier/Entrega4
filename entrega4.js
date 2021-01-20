b751529cb4e066e8252b4176e3f5b76258e2eb32
juliana <julianaclapier@gmail.com>
2021-01-20 17:25:52 -0300

    .

--- entrega4.js
+++ entrega4.js
@@ -1,11 +1,37 @@
-var resultado = multiplica(10,prompt("Multiplico este numero por 10 ydoy el promedio"))
+// var resultado = multiplica(10,prompt("Multiplico este numero por 10 ydoy el promedio"))
 
 
-function multiplica(parametro1,parametro2){
-    var resultado = ((parametro1*parametro2)/2)
+// function multiplica(parametro1,parametro2){
+//     var resultado = ((parametro1*parametro2)/2)
 
-    return(resultado)}
+//     return(resultado)}
 
-    alert("El resultado es = " + resultado);
+//     alert("El resultado es = " + resultado);
 
     
+
+
+// function Moto( modelo, precio, marca){
+
+// this.modelo = modelo;
+// this.precio = precio;
+// this.marca = marca;}
+
+// let motoUno = new Moto(2009, 80000, "motomel");
+// let motoDos = new Moto(2010, 100000, "gilera");
+// let motoTres = new Moto(2021, 200000, "yamaha");
+
+// console.log(motoUno, motoDos, motoTres);
+
+
+class moto {
+    constructor ( modelo, precio, marca){
+
+    this.modelo = modelo
+    this.precio = precio
+    this.marca = marca
+}}
+
+let mimoto= new moto(2009, 280000, "yamaha")
+
+console.log(moto)
